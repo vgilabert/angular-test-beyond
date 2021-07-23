da7cf72659ad0cc4f788b67d2e1d57c2a00d3572
vgilabert <vincent.gilabert@gmail.com>
2021-07-23 11:49:41 +0200

    3 files added

---
+++ src/app/gratitude/gratitude/gratitude.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gratitude',
+  templateUrl: './gratitude.component.html',
+  styleUrls: ['./gratitude.component.css']
+})
+export class GratitudeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
