d40a1148d96ec7382eeec2cf1cbc021af2378d10
vgilabert <vincent.gilabert@gmail.com>
2021-07-23 17:06:28 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { BubbleComponent } from './gratitude/bubble/bubble.component';
+import { GratitudeComponent } from './gratitude/gratitude/gratitude.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, BubbleComponent],
+  declarations: [AppComponent, HelloComponent, GratitudeComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
